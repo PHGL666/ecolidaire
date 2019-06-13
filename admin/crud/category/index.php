1aca87ac732497bf00ee0807bd798417dc1f29dc
Nicolas <nicolas35hermange@gmail.com>
2019-06-13 15:42:50 +0200

    Modifier fichier

--- admin/crud/category/index.php
+++ admin/crud/category/index.php
@@ -6,27 +6,14 @@ $categories = getAllRows("category");
 require_once __DIR__ . "/../../layout/header.php";
 ?>
 
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Gestion des catégories</h1>
-        <a href="create-form.php" class="btn btn-primary">
-            <i class="fa fa-plus"></i>
-            Ajouter
-        </a>
-    </div>
+    <h1>Gestion des catégories</h1>
 
-    <div>
-        <?php if (isset($_SESSION["flash"])): ?>
-            <?php foreach ($_SESSION["flash"] as $error) : ?>
-                <div class="alert alert-<?= $error["type"]; ?> alert-dismissible fade show">
-                    <?= $error["message"]; ?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            <?php endforeach; ?>
-            <?php unset($_SESSION["flash"]); ?>
-        <?php endif; ?>
-    </div>
+    <a href="create-form.php" class="btn btn-primary">
+        <i class="fa fa-plus"></i>
+        Ajouter
+    </a>
+
+    <hr>
 
     <table class="table table-striped table-bordered">
         <thead class="thead-light">
@@ -39,14 +26,20 @@ require_once __DIR__ . "/../../layout/header.php";
         <?php foreach ($categories as $category) : ?>
             <tr>
                 <td><?= $category["label"]; ?></td>
-                <td>
-                    <form action="delete-query.php" method="post">
-                        <input type="hidden" name="id" value="<?= $category["id"]; ?>">
-                        <button type="submit" class="btn btn-danger">
-                            <i class="fa fa-trash"></i>
-                            Supprimer
-                        </button>
-                    </form>
+                <td class="actions">
+                    <div class="d-flex justify-content-around">
+                        <a href="update-form.php?id=<?= $category["id"]; ?>" class="btn btn-warning">
+                            <i class="fa fa-edit"></i>
+                            Modifier
+                        </a>
+                        <form method="post" action="delete-query.php">
+                            <input type="hidden" name="id" value="<?= $category["id"]; ?>">
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa fa-trash"></i>
+                                Supprimer
+                            </button>
+                        </form>
+                    </div>
                 </td>
             </tr>
         <?php endforeach; ?>
