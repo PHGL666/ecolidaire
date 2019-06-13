2144d69c9a2ab105689738249e3595b33de6c8f0
Nicolas <nicolas35hermange@gmail.com>
2019-06-13 16:44:34 +0200

    CRUD images rétrécies+ gestion projets + entity

--- admin/layout/menu.php
+++ admin/layout/menu.php
@@ -1,14 +1,20 @@
 <ul class="nav flex-column">
     <li class="nav-item">
-        <a class="nav-link active" href="<?= SITE_ADMIN  ; ?>">
+        <a class="nav-link active" href="<?= SITE_ADMIN; ?>">
             <i class="fa fa-home"></i>
             Dashboard <span class="sr-only">(current)</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="<?= SITE_ADMIN  ; ?>crud/category/">
-            <span class="fa fa-tags"></span>
-            Catégories <span class="sr-only">(current)</span>
+        <a class="nav-link" href="<?= SITE_ADMIN; ?>crud/project/">
+            <i class="fa fa-briefcase"></i>
+            Projets
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?= SITE_ADMIN; ?>crud/category/">
+            <i class="fa fa-tags"></i>
+            Catégories
         </a>
     </li>
 </ul>
